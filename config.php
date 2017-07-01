f2a2102ac2bf43833aba2822992577ff7c9a365e
kobeuu <kobeuu@gmail.com>
2017-07-01 15:35:18 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,76 @@
+<?php
+return [
+
+  /**
+   * ------------------------------------------------------------------------
+   *  Pengaturan User
+   * ------------------------------------------------------------------------
+   *
+   *  file hasil upload akan disimpan sesuai pengaturan berikut
+   *  upload/{mapel}/{kelas}_{tugas_ke}_{nama siswa}
+   */
+
+  /*
+  |--------------------------------------------------------------------------
+  | Mata Pelajaran
+  |--------------------------------------------------------------------------
+  | Mata Pelajaran untuk kirim file
+  |
+  */
+  'mapel' => 'Simulasi Digital',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Kelas
+  |--------------------------------------------------------------------------
+  | rombongan belajar siswa
+  |
+  | misalnya 'X.1 Multimedia' atau 'XI.2 Pemasaran'
+  */
+  'kelas' => 'X.1 Multimedia',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Tugas Ke
+  |--------------------------------------------------------------------------
+  |
+  | format penulisan : 1 atau 'satu'
+  */
+  'tugas_ke' => 'satu',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Format File
+  |--------------------------------------------------------------------------
+  | format file yang diizinkan untuk dikirim
+  |
+  | misalnya 'mp4','zip','rar','pdf','doc','jpg','png'
+  */
+  'file' => 'jpg',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Ukuran Maksimal
+  |--------------------------------------------------------------------------
+  | Ukuran maksimal file yang dapat dikirim
+  |
+  | Pastikan sudah mengatur bagian berikut di file php.ini
+  | upload_max_filesize, post_max_size,
+  | max_input_time, max_execution_time
+  |
+  | ditulis dalam satuan Bytes, 1 MB = 1000000 Bytes
+  */
+  'ukuran' => 10000000,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Kirim Ulang
+  |--------------------------------------------------------------------------
+  | dapat digunakan untuk mencegah kecurangan siswa mengirim kembali file
+  | tugas yang telah direvisi
+  |
+  | true atau false
+  */
+  'kirim_ulang' => false
+
+];
---
+++ index.view.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Kirim Tugas</title>
+
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    </head>
+
+    <body>
+        <div class="container">
+          <h1>Formulir Kirim Tugas <small>| SMK Plus Al-Farhan</small></h1>
+          <hr>
+          <?php echo $flashMessage; ?>
+
+          <form class="form-horizontal" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-2 control-label">Nama Lengkap</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="nama" autofocus="autofocus" placeholder="Nama Lengkap" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-2 control-label">Kelas</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="kelas" value="<?php echo $kelas ?>" readonly>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-2 control-label">Mata Pelajaran</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="mapel" value="<?php echo $mapel ?>" readonly>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputPassword3" class="col-sm-2 control-label">File Input</label>
+              <div class="col-sm-10">
+                <input type="file" name="file">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" name="submit" value="submit"class="btn btn-default">Kirim</button>
+              </div>
+            </div>
+          </form>
+        </div>
+    </body>
+</html>
